e87562d6e5f18803163d5a159bf05c8863546801
Khiew Jian Bin <khiewbin@gmail.com>
2025-01-08 02:14:37 +0800

    add hierachy highlight

---
+++ Assets/HierachyHighlight/Editor/HierachyHighlightEditor.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HierachyHighlight))]
+[CanEditMultipleObjects]
+public class HierachyHighlightEditor : Editor
+{
+    SerializedProperty backgroundColor;
+    SerializedProperty textColor;
+    SerializedProperty fontSize;
+    SerializedProperty fontStyle;
+    SerializedProperty textAlignment;
+
+    TextAnchorMini anchor;
+
+    void OnEnable()
+    {
+        backgroundColor = serializedObject.FindProperty(nameof(HierachyHighlight.backgroundColor));
+        textColor = serializedObject.FindProperty(nameof(HierachyHighlight.textColor));
+        fontSize = serializedObject.FindProperty(nameof(HierachyHighlight.fontSize));
+        fontStyle = serializedObject.FindProperty(nameof(HierachyHighlight.fontStyle));
+        textAlignment = serializedObject.FindProperty(nameof(HierachyHighlight.textAlignment));
+
+        anchor = TextAnchorToMini((TextAnchor)textAlignment.enumValueIndex);
+    }
+    public override void OnInspectorGUI()
+    {
+
+        EditorGUILayout.PropertyField(backgroundColor, new GUIContent("Font Style"));
+        EditorGUILayout.PropertyField(textColor, new GUIContent("Font Style"));
+        EditorGUILayout.PropertyField(fontSize, new GUIContent("Font Style"));
+        EditorGUILayout.PropertyField(fontStyle, new GUIContent("Font Style"));
+
+        anchor = (TextAnchorMini)EditorGUILayout.EnumPopup("Font Alignment", anchor);
+        switch (anchor)
+        {
+            case TextAnchorMini.Left:
+                textAlignment.enumValueIndex = (int)TextAnchor.MiddleLeft;
+                break;
+
+            case TextAnchorMini.Center:
+                textAlignment.enumValueIndex = (int)TextAnchor.MiddleCenter;
+                break;
+
+            case TextAnchorMini.Right:
+                textAlignment.enumValueIndex = (int)TextAnchor.MiddleRight;
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
+        //base.OnInspectorGUI();
+    }
+
+    enum TextAnchorMini
+    {
+        Left, Center, Right
+    }
+    TextAnchorMini TextAnchorToMini(TextAnchor textAnchor)
+    {
+        switch (textAnchor)
+        {
+            case TextAnchor.UpperLeft:
+            case TextAnchor.MiddleLeft:
+            case TextAnchor.LowerLeft:
+                return TextAnchorMini.Left;
+
+            case TextAnchor.UpperCenter:
+            case TextAnchor.MiddleCenter:
+            case TextAnchor.LowerCenter:
+                return TextAnchorMini.Center;
+
+            case TextAnchor.UpperRight:
+            case TextAnchor.MiddleRight:
+            case TextAnchor.LowerRight:
+                return TextAnchorMini.Right;
+
+            default:
+                return TextAnchorMini.Center;
+        }
+    }
+}
+
---
+++ Assets/HierachyHighlight/Editor/PrettyHierarchy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class PrettyHierarchy
+{
+    static PrettyHierarchy()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowOnGUI;
+    }
+
+    static void HierarchyWindowOnGUI(int instanceID, Rect selectionRect)
+    {
+        UnityEngine.Object instance = EditorUtility.InstanceIDToObject(instanceID);
+
+        if (instance == null) return;
+
+        if (!(instance as GameObject).TryGetComponent<HierachyHighlight>(out var highlight)) return;
+
+        HierarchyItem item = new HierarchyItem(instanceID, selectionRect);
+
+        DrawHierachyItemWithHighlight(item, highlight);
+    }
+
+    static void DrawHierachyItemWithHighlight(HierarchyItem item, HierachyHighlight highlight)
+    {
+        // Background
+        Color32 bgcolor = item.IsSelected ?
+           EditorColors.GetDefaultBackgroundColor(EditorUtils.IsHierarchyFocused, item.IsSelected) :
+           highlight.backgroundColor;
+
+        EditorGUI.DrawRect(item.BackgroundRect, bgcolor);
+
+        // Child Foldout
+        if (highlight.gameObject.transform.childCount > 0)
+        {
+            Type sceneHierarchyWindowType = typeof(Editor).Assembly.GetType("UnityEditor.SceneHierarchyWindow");
+            PropertyInfo sceneHierarchyWindow = sceneHierarchyWindowType.GetProperty("lastInteractedHierarchyWindow", BindingFlags.Public | BindingFlags.Static);
+
+            int[] expandedIDs = (int[])sceneHierarchyWindowType.GetMethod("GetExpandedIDs", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(sceneHierarchyWindow.GetValue(null), null);
+
+            string iconID = expandedIDs.Contains(item.InstanceID) ? "IN Foldout on" : "IN foldout";
+
+            GUI.DrawTexture(item.CollapseToggleIconRect, EditorGUIUtility.IconContent(iconID).image);
+        }
+
+        // Icon
+        GUIContent content = EditorGUIUtility.ObjectContent(EditorUtility.InstanceIDToObject(item.InstanceID), null);
+        GUI.DrawTexture(item.PrefabIconRect, content.image);
+
+        // Text
+        Color textColor = item.IsSelected ?
+            EditorColors.GetDefaultTextColor(EditorUtils.IsHierarchyFocused, item.IsSelected) :
+            highlight.textColor;
+
+        GUIStyle labelGUIStyle = new GUIStyle
+        {
+            normal = new GUIStyleState { textColor = textColor },
+            fontStyle = highlight.fontStyle,
+            alignment = highlight.textAlignment,
+            fontSize = highlight.fontSize,
+        };
+
+        EditorGUI.LabelField(item.TextRect, highlight.name, labelGUIStyle);
+
+        // Prefab
+        if (PrefabUtility.GetCorrespondingObjectFromOriginalSource(highlight.gameObject) != null && PrefabUtility.IsAnyPrefabInstanceRoot(highlight.gameObject))
+        {
+            GUI.DrawTexture(item.EditPrefabIconRect, EditorGUIUtility.IconContent("ArrowNavigationRight").image);
+        }
+    }
+}
